e3715a7b250f9e939fe89d4a42e30454ad6670e8
nkukehenry <henricsanyu@gmail.com>
2023-11-08 18:57:18 +0300

    BC jar

--- src/main/java/com/interswitchug/phoenix/api/middleware/PhoenixSimulatorApplication.java
+++ src/main/java/com/interswitchug/phoenix/api/middleware/PhoenixSimulatorApplication.java
@@ -1,12 +1,18 @@
 package com.interswitchug.phoenix.api.middleware;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
 
 @SpringBootApplication
 public class PhoenixSimulatorApplication {
 
 	public static void main(String[] args) {
+
+		Security.addProvider(new BouncyCastleProvider());
+
 		SpringApplication.run(PhoenixSimulatorApplication.class, args);
 	}
 
